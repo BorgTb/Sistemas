fc249dda28bc913d9ae0c04afa3e9cdeb1ee5abf
BorgTb <ameza21@alumnos.utalca.cl>
2024-11-06 14:32:33 -0300

    Correccion vistaExamenes

--- src/main/java/Frontend/Vistas/VistaExamenes.java
+++ src/main/java/Frontend/Vistas/VistaExamenes.java
@@ -226,7 +226,6 @@ public class VistaExamenes extends JFrame {
             try {
                 System.out.println("Enviando mensaje: " + pestaña + ":" + mensajeFormateado);
                 salida.writeUTF(pestaña + ":" + mensajeFormateado);
-                System.out.println(entrada.readUTF());
                 campoMensaje.setText("");
             } catch (IOException e) {
                 System.err.println("Error al enviar el mensaje: " + e.getMessage());
